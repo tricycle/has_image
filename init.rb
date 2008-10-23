6048a0cb2bbcbb9757bfc9150ff101b8e344f0bf
Gerrit Kaiser <gerrit@gerritkaiser.de>
2008-10-23 17:49:30 +1100

    expanded version checks

--- init.rb
+++ init.rb
@@ -1,8 +1,19 @@
-def imagemagick_installed?
-  `which identify`
-   $?.success?
+versionstring = `Magick-config --version`
+abort "has_image needs ImageMagick, but it was not found in PATH. Is it installed? 
+  (called from #{__FILE__})" if versionstring.blank?
+
+# ImageMagick version that supports square thumbnails
+# http://www.imagemagick.org/script/command-line-options.php#resize
+minimum_version = [6,3,9]
+
+def right_imagemagick_version?(required, found)
+  actual = found.match(/^(\d)\.(\d)\.(\d)/).captures.collect(&:to_i)
+  actual[0] >= required[0] && actual[1] >= required[1] && actual[2] >= required[2]
+end
+
+unless right_imagemagick_version?(minimum_version, versionstring)
+  abort "has_image needs at least ImageMagick #{minimum_version.join('.')}. 
+    You have #{versionstring} installed. (called from #{__FILE__})"
 end
 
 require 'has_image'
-
-abort 'ImageMagick not found in PATH. Is it installed?' unless imagemagick_installed?
