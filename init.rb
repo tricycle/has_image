5092c99245d96051794219e32f897b32934257f9
Gerrit Kaiser <gerrit@gerritkaiser.de>
2008-10-23 18:48:05 +1100

    be slightly less naïve about comparing versions

--- init.rb
+++ init.rb
@@ -8,7 +8,9 @@ minimum_version = [6,3,9]
 
 def right_imagemagick_version?(required, found)
   actual = found.match(/^(\d)\.(\d)\.(\d)/).captures.collect(&:to_i)
-  actual[0] >= required[0] && actual[1] >= required[1] && actual[2] >= required[2]
+  actual[0] > required[0] ||
+    actual[0] == required[0] && actual[1] >= required[1] ||
+    actual[0] == required[0] && actual[1] == required[1] && actual[2] >= required[2]
 end
 
 unless right_imagemagick_version?(minimum_version, versionstring)
